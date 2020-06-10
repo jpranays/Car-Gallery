eaf9d2f2b56a778bd44df5110f6000cbcd0e6e5a
Pranay <63608934+jpranays@users.noreply.github.com>
2020-06-10 08:53:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,33 @@
 window.addEventListener("load", () => {
 	document.querySelector(".loader").style.display = "none";
-// 	document.querySelector(".container").style.animationDelay = "4s";
-// 	let counter = setTimeout(() => {
-// 		counter = setInterval(anim, 100);
-// 	}, 500);
-// 	inc = 0;
-// 	function anim() {
-// 		let image = document.querySelectorAll("img")[inc];
-// 		inc++;
-// 		image.style.transform = "rotate(360deg)";
-// 		if (inc == 13) {
-// 			clearInterval(counter);
-// 		}
-// 	}
+	let container = document.querySelector(".container");
+	// 	document.querySelector(".container").style.animationDelay = "4s";
+	// 	let counter = setTimeout(() => {
+	// 		counter = setInterval(anim, 100);
+	// 	}, 500);
+	// 	inc = 0;
+	// 	function anim() {
+	// 		let image = document.querySelectorAll("img")[inc];
+	// 		inc++;
+	// 		image.style.transform = "rotate(360deg)";
+	// 		if (inc == 13) {
+	// 			clearInterval(counter);
+	// 		}
+	// 	}
 	setInterval(function () {
 		$("#mycar").shake();
 	}, 3000);
-
+	let i = 0;
 	$.shake({
 		callback: function () {
-			alert("Hurray");
+			i++;
+			navigator.vibrate(200);
+			if ($(container).css("animation-play-state") == "paused") {
+				$(container).css("animation-play-state", "running");
+			}
+			if (i == 10) {
+				alert("gotcha");
+			}
 		},
 	});
 });
