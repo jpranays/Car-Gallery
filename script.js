584c23954d06411f589b1faee301ac15f36ce123
Pranay <63608934+jpranays@users.noreply.github.com>
2020-06-10 08:22:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
-window.addEventListener("load", () => {
-	document.querySelector(".loader").style.display = "none";
-	document.querySelector(".container").style.animationDelay = "4s";
-	let counter = setTimeout(() => {
-		counter = setInterval(anim, 100);
-	}, 500);
-	inc = 0;
-	function anim() {
-		let image = document.querySelectorAll("img")[inc];
-		inc++;
-		image.style.transform = "rotate(360deg)";
-		if (inc == 13) {
-			clearInterval(counter);
-		}
-	}
+// window.addEventListener("load", () => {
+// 	document.querySelector(".loader").style.display = "none";
+// 	document.querySelector(".container").style.animationDelay = "4s";
+// 	let counter = setTimeout(() => {
+// 		counter = setInterval(anim, 100);
+// 	}, 500);
+// 	inc = 0;
+// 	function anim() {
+// 		let image = document.querySelectorAll("img")[inc];
+// 		inc++;
+// 		image.style.transform = "rotate(360deg)";
+// 		if (inc == 13) {
+// 			clearInterval(counter);
+// 		}
+// 	}
+// });
+$(document).ready(function () {
+	setInterval(function () {
+		$("#mycar").shake();
+	}, 3000);
+
+	$.shake({
+		callback: function () {
+			alert("Hurray");
+		},
+	});
 });
