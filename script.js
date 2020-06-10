6df35c89079c08218e99afc017815efec0ff2164
Pranay <63608934+jpranays@users.noreply.github.com>
2020-06-10 10:22:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 window.addEventListener("load", () => {
 	document.querySelector(".loader").style.display = "none";
 	let container = document.querySelector(".container");
-
-	setInterval(function () {
+	$(".container > img").css("animation-play-state", "running");
+	let middleCar = setInterval(function () {
 		$("#mycar").shake();
 	}, 3000);
-
 	let i = 0;
 	$.shake({
 		callback: function () {
@@ -16,13 +15,13 @@ window.addEventListener("load", () => {
 					$(container).css("animation-play-state", "running");
 					setTimeout(() => {
 						$(container).css("animation-play-state", "paused");
-					}, 3000);
+					}, 2500);
 				}
 			}
 			if (i == 3) {
+				clearInterval(middleCar);
 				alert("Spread");
 				$(container).css("animation", "none");
-				$(".container > img").css("animation-play-state", "running");
 				counter = setInterval(anim, 100);
 				inc = 0;
 
